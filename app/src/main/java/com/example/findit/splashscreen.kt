1ee5ed44c946d9d1f9e0e3c122a2a715b04723ef
cyraalesha <67356675+cyraalesha@users.noreply.github.com>
2020-08-03 01:10:33 +0700

    Filesss

---
+++ app/src/main/java/com/example/findit/splashscreen.kt
@@ -0,0 +1,28 @@
+package com.example.findit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.findit.ui.login.loginpage
+
+class splashscreen : AppCompatActivity() {
+    lateinit var handler: Handler
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        handler = Handler()
+        handler.postDelayed({
+
+            val intent =
+        Intent(this, loginpage::class.java)
+            startActivity(intent)
+            finish()
+
+
+        }, 3000)
+    }
+}
